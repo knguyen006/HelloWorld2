6796e73baee0e6cf5e676d3413c53094d8606e58
knguyen006 <knguyen006@regis.edu>
2013-05-16 19:18:01 -0600

    Week 2

---
+++ HelloWorld2/HelloWorld2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Created by PNguyen 5/15/2013
+//MSSE680 Week 2 - Discussion #1
+//
+using System;
+
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
